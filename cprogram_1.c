06ba818b42686d8693d9273f5d7d3f53f24b000f
manoj0408-prog <livechic0408@gmail.com>
2026-06-02 12:33:31 +0530

    Update cprogram_1.c

--- cprogram_1.c
+++ cprogram_1.c
@@ -3,8 +3,8 @@ int main()
 {
     int a,b;
     printf("enter the number:");
-    scanf(%d,&a);
-    a=a+2;
-    printf("the no after adding:",a);
+    scanf("%d",&a);
+    b=a+2;
+    printf("the no after adding:%d",b);
     return 0;
 }
