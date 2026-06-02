d52b161021867dde42f96ebe886b09040cff4e73
manoj0408-prog <livechic0408@gmail.com>
2026-06-02 21:19:12 +0530

    Add files via upload

---
+++ cprogram_24.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num, result;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    result = num - 5 * (((num/100 )*(num%10))% 2);
+    printf("Result = %d", result);
+    return 0;
+}
